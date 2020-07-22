1549bd1580d0d2d20420d24cb73e49e64d8b4f05
Eloy <eloydgutierrez@gmail.com>
2020-07-22 19:28:13 -0400

    fixed users file

--- data/seeds/01_users.js
+++ data/seeds/01_users.js
@@ -5,8 +5,8 @@ exports.seed = async function(knex) {
   const hashedPass = await bcrypt.hash("123", 10)
 
   await knex("users").insert([
-    {username: "Jane", password: hashedPass},
-    {username: "James", password: hashedPass},
-    {username: "Dan", password: hashedPass},
+    {username: "Jane", password: hashedPass, phoneNumber: "111-111-111"},
+    {username: "James", password: hashedPass, phoneNumber: "222-222-222"},
+    {username: "Dan", password: hashedPass, phoneNumber: "333-333-333"},
   ]);
 };
