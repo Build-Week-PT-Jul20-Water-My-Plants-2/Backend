4898c0e3f50679a2de392e2ecfe542f9ff6b4b6d
Eloy <eloydgutierrez@gmail.com>
2020-07-22 10:14:20 -0400

    updated users table. added plants and users_plants table

--- data/migrations/20200721220525_users.js
+++ data/migrations/20200721220525_users.js
@@ -1,11 +1,37 @@
 exports.up = async function (knex) {
   await knex.schema.createTable("users", (table) => {
-    table.increments();
+    table.increments("id");
     table.string("username").unique().notNull();
     table.string("password").notNull();
+    table.string("phoneNumber").notNull();
+  })
+  .createTable("plants", (table) => { 
+    table.increments("id");
+    table.string("nickname").notNull();
+    table.string("species").notNull();
+    table.string("h2oFrequency").notNull();
+  })
+  .createTable("users_plants", (table) => {
+    table.integer("user_id")
+          .unsigned()
+          .notNull()
+          .references("id")
+          .inTable("users")
+          .onUpdate('CASCADE')
+          .onDelete('CASCADE')
+    table.integer("plant_id")
+          .unsigned()
+          .notNull()
+          .references("id")
+          .inTable("plants")
+          .onUpdate('CASCADE')
+          .onDelete('CASCADE')
   });
 };
 
 exports.down = async function (knex) {
-  await knex.schema.dropTableIfExists("users");
+  await knex.schema
+  .dropTableIfExists("users_plants")
+  .dropTableIfExists("plants")
+  .dropTableIfExists("users")
 };
