5c56fa9178e9004462accc4be1c6ff39d69358fa
Eloy <eloydgutierrez@gmail.com>
2020-07-22 13:15:16 -0400

    updated register and login endpoint. added plant CRUD functions and update user function to users-model. added CRUD endpoints for plants and update user endpoint. added .gitignore file to ignore .env file

--- users/users-model.js
+++ users/users-model.js
@@ -9,11 +9,52 @@ function find() {
 }
 
 function findByFilter(filter) {
-  return db("users").select("id", "username").where("username", filter).first();
+  return db("users").select("id", "username", "phoneNumber").where("username", filter).first();
+}
+
+async function updateUser(changes, id) { // updates user and returns updated user object
+  await db("users").update(changes).where("id", id)
+  return await db.first("*").from("users").where("id", id).select("id", "username", "phoneNumber")
+}
+
+// PLANT CRUD FUNCTIONS BELOW //////////////////////////////////////////////////////////////////////////////////////////////
+
+function getUserPlants(id) { // returns list of plants a user has created
+  return db("plants")
+        .innerJoin("users_plants as up", "up.plant_id", "plants.id")
+        .where("up.user_id", id)
+        .select("plants.id as id", "plants.nickname", "plants.species", "plants.h2oFrequency")
+}
+
+async function addPlant(object) { // adds new plant to database and returns newly created plant object
+  const [newPlantId] = await db.insert(object).into("plants")
+  const newPlant = await db.first("*").from("plants").where("id", newPlantId)
+  return newPlant
+}
+
+async function updatePlant(changes, id) { // updates plant and returns updated plant object
+  await db("plants").update(changes).where("id", id)
+  return await db.first("*").from("plants").where("id", id)
+}
+
+async function removePlant(id) { // deletes plant object and returns deleted plant object. If no plant found with that ID, returns null.
+  const deleted = await db.first("*").from("plants").where("id", id)
+  if (deleted) {
+      await db("plants").where("id", id).del()
+      return deleted
+  } else {
+      return null
+  }
+
 }
 
 module.exports = {
   add,
   find,
   findByFilter,
+  updateUser,
+  getUserPlants,
+  addPlant,
+  updatePlant,
+  removePlant,
 };
