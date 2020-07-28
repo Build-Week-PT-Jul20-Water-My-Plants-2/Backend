c1710f555036277d640a4fb88f3080eb5a94745b
Eloy <eloydgutierrez@gmail.com>
2020-07-28 00:07:13 -0400

    reverted last merge

--- api/server.js
+++ api/server.js
@@ -3,19 +3,15 @@ const cookieParser = require("cookie-parser");
 const welcomeRouter = require("../welcome/welcome-router");
 const usersRouter = require("../users/users-router");
 const authRouter = require("../auth/auth-router");
-const cors = require('cors')
-const helmet = require('helmet')
 
 const server = express();
 
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   next();
-// })
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+})
 
 server.use(express.json());
-server.use(helmet())
-server.use(cors())
 server.use(cookieParser());
 server.use("/", welcomeRouter);
 server.use("/api/users", usersRouter);
