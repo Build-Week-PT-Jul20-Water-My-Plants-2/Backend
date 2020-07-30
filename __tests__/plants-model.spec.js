12d70c675c4bcd684514458017b1cd467942169f
nuvallo <nuvallo.inc@gmail.com>
2020-07-30 19:34:34 -0400

    unit tests for plant model

--- __tests__/plants-model.spec.js
+++ __tests__/plants-model.spec.js
@@ -1,7 +1,10 @@
 const plantsModel = require("../plants/plants-model");
 
 describe("plants-model unit tests", () => {
-  it("will find and return a specific user plants", () => {
-    expect(plantsModel.getUserPlants(1)).toBeDefined();
+  it("will find and return a specific user plants", async () => {
+    const plants = await plantsModel.getUserPlants(1);
+    console.log(plants);
+    expect(plants[0].id).toBeDefined();
+    expect(plants[0].nickname).toBe("Rose");
   });
 });
--- __tests__/users-model.spec.js
+++ __tests__/users-model.spec.js
@@ -8,7 +8,6 @@ describe("users-model unit tests", () => {
     expect(users[0].username).toBe("Jane");
     expect(users[1].id).toBeDefined();
     expect(users[1].username).toBe("James");
-    expect(users[1].phoneNumber).toBe("222-222-2222");
-    console.log(users);
+    expect(users[1].phoneNumber).toBe("222-222-222");
   });
 });
